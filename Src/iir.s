64a141bc1295a3aec235b52b576bf278cf4f7355
VINCELAPTOP\sumen <sumenghang@gmail.com>
2025-09-12 00:15:15 +0800

    Added comments abnd cleaned up code

--- Src/iir.s
+++ Src/iir.s
@@ -49,6 +49,10 @@ iir:
 
 	BX LR
 
+@R4 Value of i
+@R5 Address of i
+@R10 counter j starting at 1
+@R12 Result of part 1 of arithmetic operation
 SUBROUTINE:
 	PUSH {R4-R12} @Save registers 4 to 12
 	LDR R4, [R1] @Loads b[0] into R4
@@ -57,16 +61,23 @@ SUBROUTINE:
 	MUL R6, R3, R4 @R6 = x_n * b[0]
 	SDIV R12, R6, R5 @R6 = x_n * b[0] / a[0]
 
-	SUB R7, R0, #1 @Loop counter where j = n-1
+	SUB R7, R0, #1 @Loop counter where k = N-1
 	LDR R5, =i @R5 stores address of i
 	LDR R4, [R5] @R4 stores value of i
 
 	LDR R10, =1 @j counter starts at 1
 
+@R4 Counter i
+@R5 Address of x_array
+@R6 x_b term
+@R7 Loop counter k which starts at N-1
+@R9 Address of y_array, y_a term
+@R10 Counter j
+@R11 Counter storing value of i-1
 LOOP:
 
 	CMP R7, #0 @Compare loop counter with 0
-	BLT EXIT @Exits loop if less than or equals 0
+	BLT EXIT @Exits loop if less than 0
 
 	SUB R11, R4, #1 @Exits loop if index less than 0 to avoid illegal memory access
 	CMP R11, #0 @R11 stores i-1
@@ -75,23 +86,34 @@ LOOP:
 	LDR R5, =x_array @R5 stores address of x_array
 	LDR R9, =y_array @R9 stores address of y_array
 
-	LDR R6, [R5, R11, LSL #2] @Loads x_array[i] into R6
+	LDR R6, [R5, R11, LSL #2] @Loads x_array[i-1] into R6
 	LDR R8, [R1, R10, LSL #2] @Loads b[j] into R8, counter starts at 1 and counts to 4
 	MUL R6, R6, R8 @Get b and x term
 
-	LDR R9, [R9, R11, LSL #2] @Loads y_array[i] into R9
+	LDR R9, [R9, R11, LSL #2] @Loads y_array[i-1] into R9
 	LDR R8, [R2, R10, LSL #2] @Loads a[j] into R8, counter starts at 1 and counts to 4
 	MUL R9, R9, R8 @Get a and y term
 
 	SUB R6, R6, R9
 	LDR R11, [R2] @Loads a[0] into R11
-	SDIV R6, R6, R11
-	ADD R12, R12, R6
+	SDIV R6, R6, R11 @Divides result by a[0]
+	ADD R12, R12, R6 @Add the results together
 
-	SUB R7, R7, #1
+	SUB R7, R7, #1 @Decrement k counter
 	SUB R4, #1 @Decrement i counter
 	ADD R10, #1 @Increment j counter
 	B LOOP @Go back to loop
+
+@R0 y_n
+@R1 100
+@R3 x_n
+@R4 Value of i
+@R5 Address of i
+
+@R7 Address of x_array
+@R8 Address of y_array
+
+
 EXIT:
 
 	LDR R7, =x_array
